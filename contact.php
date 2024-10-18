a3c0677d6dca779964da8eb4b05bffbf8764ca66
Avinah Waghmare <avinashdw2910@gmail.com>
2024-10-18 10:41:09 +0530

    Avinash-portfilio

--- contact.php
+++ contact.php
@@ -1,51 +1,79 @@
 <?php
-$errors = [];
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get POST data
-    $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
-    $email = isset($_POST['email']) ? trim($_POST['email']) : '';
-    $message = isset($_POST['message']) ? strip_tags(trim($_POST['message'])) : '';
 
-    // Validate form fields
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+   
 
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
 
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
+$to = "avinashdw2910@gmail.com";
+$subject = "HTML email";
 
-    // If no errors, send email
-    if (empty($errors)) {
-        // Recipient email address (replace with your own)
-        $recipient = "avinashdw2910@gamil.com";
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>Name&#58; $name</tr>
+<tr>Email&#58; $email</tr>
+<tr>Phone&#58; $contact</tr>
 
-        // Additional headers
-        $headers = "From: $name <$email>";
+</tr>
+</table>
+</body>
+</html>
+";
 
-        // Send email
-        if (mail($recipient, $message, $headers)) {
-            echo "Email sent successfully!";
-        } else {
-            echo "Failed to send email. Please try again later.";
-        }
-    } else {
-        // Display errors
-        echo "The form contains the following errors:<br>";
-        foreach ($errors as $error) {
-            echo "- $error<br>";
-        }
-    }
-} else {
-    // Not a POST request, display a 403 forbidden error
-    header("HTTP/1.1 403 Forbidden");
-    echo "You are not allowed to access this page.";
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <avinashdw22@gmail.com>' . "\r\n";
+$headers .= 'Cc: ' . "\r\n";
+
+
+
+if(mail($to,$subject,$message,$headers)) //Send an Email. Return true on success or false on error
+{
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://connect.leadrat.com/api/v1/integration/GoogleAds');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n  \"name\": \"".$name."\",\n  \"email\": \"".$email."\",\n  \"countryCode\": \"91\",\n  \"mobile\": \"".$contact."\",\n  \"submittedDate\": \"".date('d-m-y')."\"}");
+    
+    $headers = array();
+    $headers[] = 'Api-Key: bfbf5f5c8e745774aa0404baf6f80915';
+    $headers[] = 'Content-Type: application/json';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }else
+    {
+        $res=json_decode($result,true);
+        if(isset($res['succeeded'])==1)
+        echo "submitted to lead";
+        else
+        echo "Failed to lead";
+    }   
+echo 
+
+"<script>window.location.href='thank-you.html';</script>";
+}
+else
+{
+echo "<script>
+alert('Plz Try Agian');
+window.location.href='index.html'
+
+;
+</script>";
 }
 ?>
